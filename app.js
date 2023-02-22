82224a548807eb99c7e9ac12585b9936c3e13d94
MosesK <mosesziongo@gmail.com>
2023-02-22 13:33:59 +0200

    resolution du bug lié aux trasitions

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const sectBtns = document.querySelectorAll('.controlls');
 const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
 
+
 function PageTransitions(){
     for(let i=0 ; i < sectBtn.length ; i++){
         sectBtn[i].addEventListener('click', function(){
             let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className= currentBtn[0].className.replace('active-btn', '');
-            this.className += ' active-btn'
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
         });
     }
 
@@ -27,6 +28,6 @@ function PageTransitions(){
             const element = document.getElementById(id);
             element.classList.add('active');
         }
-    })
+    });
 }
 PageTransitions();
